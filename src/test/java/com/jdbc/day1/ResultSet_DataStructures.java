73e9b60884d9d40cdfeceaafa95cc32fac504914
jaminadi <jama.nadyrbek@gmail.com>
2022-06-21 08:48:27 -0500

    Added JDBC classes by M

--- src/test/java/com/jdbc/day1/ResultSet_DataStructures.java
+++ src/test/java/com/jdbc/day1/ResultSet_DataStructures.java
@@ -1,9 +1,12 @@
 package com.jdbc.day1;
 
 import com.jdbc.utilities.DBUtility;
+import org.junit.Assert;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResultSet_DataStructures {
 
@@ -11,13 +14,23 @@ public class ResultSet_DataStructures {
 
         ResultSet result = DBUtility.getResult("SELECT * FROM employees");
 
+        List<String> employeeNames = new ArrayList<>();
+
         //we want to get first names and last names of the employees
         while (result.next()) {
             String firstName = result.getString("first_name");
             String lastName = result.getString("last_name");
 
             String fullName = firstName + " " + lastName;
-            System.out.println(fullName);
+            employeeNames.add(fullName);
         }
+
+        System.out.println(employeeNames);
+
+        //we need to verify that name Steven King exists in the employees database
+        //Assert.assertTrue(employeeNames.contains("Steven King"));
+        System.out.println(employeeNames.contains("Steven King"));
+
+        DBUtility.tearDown();
     }
 }
---
+++ src/test/java/com/jdbc/day1/ResultSet_Map.java
@@ -0,0 +1,39 @@
+package com.jdbc.day1;
+
+import com.jdbc.utilities.DBUtility;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ResultSet_Map {
+
+    public static void main(String[] args) throws SQLException {
+
+        ResultSet result = DBUtility.getResult("SELECT * FROM employees");
+        Map<String, Integer> employeeInfo = new LinkedHashMap<>(); //because LinkedHashMap keeps the result in the original order
+
+        //find out the name of employee who makes 9000
+        //String nameOfEmployee9000 = "";
+        while (result.next()) {
+            String firstName = result.getString("first_name");
+            String lastName = result.getString("last_name");
+
+            //here, we got the employee names
+            String fullName = firstName + " " + lastName;
+            //now we need to get salary of the employees
+            int salary = result.getInt("salary");
+
+            employeeInfo.put(fullName, salary);
+
+//            if (salary == 9000){
+//                System.out.println(fullName + " is making 9000");
+//            }
+        }
+        System.out.println(employeeInfo);
+
+        System.out.println(employeeInfo.get("Steven King") == 24000);
+
+    }
+}
